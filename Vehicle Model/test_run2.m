80f831fad1335e76819aa68e5d21f48c4c138793
Wing-ka-king <Wing-ka-king@users.noreply.github.com>
2019-01-18 16:23:08 +0100

    Add files via upload
    input command is now from matlab dataset

--- Vehicle Model/test_run2.m
+++ Vehicle Model/test_run2.m
@@ -2,13 +2,15 @@ clc
 clear all
 close all
 
-dt = 0.01;
-slip_FL = 0.0015;
-slip_FR = 0.0015;
-slip_RR = 0;
-slip_RL = 0;
-steerAngle = 0.001;
-u = [slip_FL slip_FR slip_RL slip_RR steerAngle];
+dt = 0.1;
+% slip_FL = 0.0015;
+% slip_FR = 0.0015;
+% slip_RR = 0;
+% slip_RL = 0;
+% steerAngle = -0.01;
+% u = [slip_FL slip_FR slip_RL slip_RR steerAngle];
+load(fullfile(matlabroot, 'toolbox', 'ident', 'iddemos', 'data', 'vehicledata'));
+u = u1;
 
 x = [0,0,0];
 %       m  = p(1);   /* Vehicle mass.                    */
@@ -19,18 +21,15 @@ x = [0,0,0];
 %       CA = p(6);   /* Air resistance coefficient.      */
 param = [1700 1.5 1.5 1.5e5 4e4 0.5];
 
-% pos_x = 0;
-% pos_y = 0;
 pos = [0 0 0];
-N = 200;
+N = size(u1,1);
+
 for i=1:N
     plot(pos(1),pos(2),'*r');    
-    delta = compute_dx(x(i,:),u,param,dt);
-    x(i+1,:) = x(i,:) + delta*dt;
-%     slipAngle_F = u(5) - atan2(x(i,2)+param(2)*x(i,3),x(i,1));
-%     slipAngle_R = atan2(param(3)*x(i,3)-x(i,2),x(i,1));
-%     slip(i,:) = [slipAngle_F, slipAngle_R];
-    pos = pos + x(i,:);
+    delta = compute_dx(x(i,:),u(i,:),param);
+    x(i+1,:) = x(i,:) + delta*dt ;
+
+    pos = pos + x(i,:)*dt;
     hold on;
     %drawnow; 
     %pause(0.5);
